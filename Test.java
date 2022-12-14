2be6bcc0004ea83c7ffcbdd052f1d455271b0bd9
sravanreddy278 <99219797+sravanreddy278@users.noreply.github.com>
2022-12-14 14:21:00 -0600

    Update Test.java
    changed a = 30

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ class Test1
 {
 public static void main(String[] args)
 {
-int a=20;
+int a=30;
 System.out.print("Hello Repo12");
   System.out.println("Remotely Edited 1");
   System.out.println(a);
