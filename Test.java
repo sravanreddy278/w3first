c2b86d6623519eae8819e24190889c84e1c926d9
Sravan Reddy Gundeti <sravan>
2022-12-14 13:00:19 -0600

    declared variable a

--- Test.java
+++ Test.java
@@ -2,9 +2,10 @@ class Test1
 {
 public static void main(String[] args)
 {
-
+int a=20;
 System.out.print("Hello Repo12");
   System.out.println("Remotely Edited 1");
+  System.out.println(a);
 
 }
 }
